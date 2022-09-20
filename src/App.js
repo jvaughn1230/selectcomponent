2194f454bbd75603408d870c9a37926ec703a336
Jeffrey Vaughn <jvaughn1230@gmail.com>
2022-09-20 16:35:39 -0700

    init commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <h1>Select Component Practice</h1>
         </a>
       </header>
     </div>
